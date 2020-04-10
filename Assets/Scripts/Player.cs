1f161a9ee768cec3f192f81dec836620c0d2417b
marcorod94 <marco.rod.94@live.com>
2020-04-11 00:24:06 +0200

    Player Creation
    Creation of the player with its basic moves

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lastPosition = transform.position;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * speed * Time.deltaTime);
+        if ((transform.position.x > 6.25F || transform.position.x < -6.25F) || (transform.position.y > 0 || transform.position.y < -4.30F))
+        {
+            transform.position = lastPosition;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# unity-galaxy-shooter
+# Galaxy Shooter
+
+This game takes the player trough an adventure in outer space here he has to shoot enemies to survive.
+
+## Author (Marco Rodriguez)
+I’m a Software Engineer from Ecuador. I enjoy to code and I love learning new programing languages. Currently I’m very interested on game development.
+
+Skills:
+
+- C++
+- C#
+- Dart
+- Flutter
+- Java
+- Javascript
+- Kotlin
+- PHP
+- Swift
+- Typescript
+
+You can find some of my projects [here.](https://github.com/marcorod94)
